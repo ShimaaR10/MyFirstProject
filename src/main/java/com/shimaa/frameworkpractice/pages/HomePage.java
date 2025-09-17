fad9c534ed0adc2a534999cc171642f202d3d2ac
Shimaa Rady <shimaasayed689@gmail.com>
2025-09-18 00:29:56 +0300

    Add Waits

--- src/main/java/com/shimaa/frameworkpractice/pages/HomePage.java
+++ src/main/java/com/shimaa/frameworkpractice/pages/HomePage.java
@@ -1,10 +1,10 @@
 package com.shimaa.frameworkpractice.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,46 +31,55 @@ public class HomePage {
 
     public void clickRegister() {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(registerLink));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        try { element.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element); }
+        try {
+            element.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 
     public void clickLogin() {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(loginLink));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        try { element.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element); }
+        try {
+            element.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 
     public void clickLogout() {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(logoutLink));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        try { element.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element); }
+        try {
+            element.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 
     public void searchProduct(String productName) {
         WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(searchBox));
         searchInput.clear();
         searchInput.sendKeys(productName);
-
-        WebElement searchBtn = wait.until(ExpectedConditions.elementToBeClickable(searchButton));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", searchBtn);
-        try { searchBtn.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", searchBtn); }
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
     }
 
     public void goToMyAccount() {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(myAccountLink));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        try { element.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element); }
+        try {
+            element.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 
     public void closeNotificationIfPresent() {
         try {
             WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(notificationBar));
             WebElement closeBtn = notification.findElement(closeNotificationBtn);
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", closeBtn);
-            try { closeBtn.click(); } catch (Exception e) { ((JavascriptExecutor) driver).executeScript("arguments[0].click();", closeBtn); }
+            closeBtn.click();
             wait.until(ExpectedConditions.invisibilityOf(notification));
         } catch (TimeoutException e) {
+            // Notification not present; do nothing
         }
     }
 }
